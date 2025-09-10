2edefdeb0b08d687ec838b4207ed4eceb1dbd89a
Fl0-1 <flolevaill@gmail.com>
2025-09-10 10:34:21 +0200

    Initial commit: 0x00-java-hello_world tasks

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat{public static void main(String[] args){
+String text1="Holberton";
+String text2="School";
+String space=" ";
+String both=text1+space+text2;
+String output="Welcome to "+both+" !";
+System.out.println(output);
+}}
