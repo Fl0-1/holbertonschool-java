4c374c002d0ec22f8c3a9efa43bf5ee1c1d31bb7
Fl0-1 <flolevaill@gmail.com>
2025-09-10 10:43:23 +0200

    git add 0x00-java-hello_world/PrintStringConcat.java

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,8 +1,10 @@
-public class PrintStringConcat{public static void main(String[] args){
-String text1="Holberton";
-String text2="School";
-String space=" ";
-String both=text1+space+text2;
-String output="Welcome to "+both+" !";
-System.out.println(output);
-}}
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton";
+        String text2 = "School";
+        String space = " ";
+        String both = text1 + space + text2;
+        String output = "Welcome to " + both + " !";
+        System.out.println(output);
+    }
+}
